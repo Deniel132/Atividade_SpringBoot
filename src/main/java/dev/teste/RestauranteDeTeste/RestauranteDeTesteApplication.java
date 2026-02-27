0b751679aac44653690e8b2e3a4803ff858a2395
Deniel132 <danielseron195@gmail.com>
2026-02-27 09:45:19 -0300

    Primeiro Commit

---
+++ src/main/java/dev/teste/RestauranteDeTeste/RestauranteDeTesteApplication.java
@@ -0,0 +1,13 @@
+package dev.teste.RestauranteDeTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteDeTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteDeTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/teste/RestauranteDeTeste/RestauranteDeTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.teste.RestauranteDeTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteDeTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
